981182d1e68301b802c30ab631350f63507d1d2b
Raka Satria Efendi <118404166+rakasatriaefendi@users.noreply.github.com>
2025-08-01 10:57:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ disease_info = {
 # ====== [LOAD MODEL] ======
 @st.cache_resource
 def load_model():
-    return tf.keras.models.load_model("trained_model.h5")
+    return tf.keras.models.load_model("trained_model.keras")
 model = load_model()
 
 # ====== [UI - JUDUL & UPLOAD] ======
