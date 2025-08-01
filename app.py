ef77f481554d88d660f951b6466ae47de45d85a3
xurobaebae <rakaefendi1683@gmail.com>
2025-08-01 11:04:10 +0700

    Modify app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def load_model():
     except Exception as e:
         st.error(f"Error loading model: {e}")
         return None
-    
+        
 model = load_model()
 
 # ====== [UI - JUDUL & UPLOAD] ======
